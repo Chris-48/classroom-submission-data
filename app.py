11f2c6a7a49719b2ec3a6fe08a998b99eb17253b
Chris-48 <christianmgoes@gmail.com>
2021-03-26 20:52:36 -0300

    created the layout of the page

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import render_template
 from flask import Flask
+from flask import request
+from flask_session import Session
+from tempfile import mkdtemp
+
 
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route("/")
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("index.html")
